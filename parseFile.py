4591ed4237a194a559c8898e7bf8b37aace630b0
andrewchabot <afc1755@rit.edu>
2020-04-25 15:42:31 -0400

    parsing files created and being run

---
+++ parseFile.py
@@ -0,0 +1,31 @@
+import string
+
+txtList = open('caa-rechecking-incident-affected-serials.txt', 'r')
+outFile = open('urls.txt', 'w')
+
+names = False
+currTxt = txtList.readline()
+nameList = []
+currName = ""
+i = 0
+while(currTxt):
+    i += 1
+    for charNum in range(0, len(currTxt)):
+        if currTxt[charNum] == '[':
+            names = True
+        elif currTxt[charNum] == ']':
+            names = False
+        elif names:
+            if not currTxt[charNum] in string.whitespace:
+                currName += currTxt[charNum]
+            else:
+                nameList.append(currName)
+                currName = ""
+    if i % 10000 == 0:
+        print(i)
+
+for name in nameList:
+    outFile.write(name + '\n')
+
+
+txtList.close()
